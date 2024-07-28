d63589636dc33c8e5ff78a56dfbc18dda96215b8
Anonstry <anonstry@protonmail.com>
2024-07-28 14:17:37 -0300

    hotfix: APP_NAME → TELEGRAM_SESSION_NAME

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,7 @@ from telethon import TelegramClient
 from telethon.types import Message
 
 
-telegram_client_session_file_path = str(Path(gettempdir()) / settings.APP_NAME)
+telegram_client_session_file_path = str(Path(gettempdir()) / settings.TELEGRAM_SESSION_NAME)
 telegram_client = TelegramClient(
     telegram_client_session_file_path,
     api_id=settings.TELEGRAM_API_ID,
@@ -55,3 +55,32 @@ class DownloadableMedia:
             formatting_entities=self.from_message.entities,
             file=str(self.media_path),
         )
+
+
+from dynaconf import settings
+from hydrogram import Client
+
+from git.repo import Repo
+
+
+from pathlib import Path
+
+DOWNLOAD_DIR = Path.home() / "Downloads"
+DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+client: Client = Client(
+    name=settings.TELEGRAM_APP_NAME,
+    api_id=settings.TELEGRAM_API_ID,
+    api_hash=settings.TELEGRAM_API_HASH,
+    bot_token=settings.TELEGRAM_BOT_TOKEN,
+    plugins=dict(root="src.modules", exclude=["experimental"]),
+)
+
+git_repository = Repo(search_parent_directories=True)
+# git_repository_remote_link = git_repository.remotes[0].config_reader.get("url")
+latest_git_repository_tag = sorted(
+    git_repository.tags, key=lambda tag: tag.commit.committed_datetime
+)[-1].name
+latest_git_repository_commit_shorted = git_repository.git.rev_parse(
+    git_repository.head.commit.hexsha, short=True
+)
