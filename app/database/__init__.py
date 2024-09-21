5c68f247ef37f67e2ce4759fc12694ecf1380e94
Anonstry <anonstry@protonmail.com>
2024-09-21 02:26:54 -0300

    removendo mongoengine e adicionando pymongo

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,13 +1,10 @@
 import secrets
 
-# import mongoengine
 from dynaconf import settings
 from pymongo import MongoClient
 
 mongo_client = MongoClient(settings.MONGO_CONNECTION_STRING)
 mongo_database = mongo_client[settings.MONGO_DATABASE_NAME]
-# mongoengine.connect("app", host=settings.MONGO_CONNECTION_STRING)
-
 
 def create_token(size=16):
     return secrets.token_hex(size)
