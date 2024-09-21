6c7b69921b16fcbfa368b4c608db016d438a27f1
Anonstry <anonstry@protonmail.com>
2024-09-21 02:27:55 -0300

    removendo criação de tokens

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,10 +1,5 @@
-import secrets
-
 from dynaconf import settings
 from pymongo import MongoClient
 
 mongo_client = MongoClient(settings.MONGO_CONNECTION_STRING)
 mongo_database = mongo_client[settings.MONGO_DATABASE_NAME]
-
-def create_token(size=16):
-    return secrets.token_hex(size)
