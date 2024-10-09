aefe75f8ca75328f8882d84a62356622bea1a1cf
Anonstry <anonstry@protonmail.com>
2024-10-09 04:16:47 -0300

    movendo arquivos python para um diretório separado

--- app/_python/information.py
+++ app/_python/information.py
@@ -18,16 +18,3 @@ async def chat_identifier(client: Client, message: Message):
 @Client.on_message(filters.outgoing & filters.command("location", "?"))
 async def location_identifier(client: Client, message: Message):
     await message.edit(f"{message.chat.id}:{message.reply_to_message_id}")
-
-
-# async def parse_location(event):
-#     command = event.text.split()
-#     link = URL(command[1])
-#     if 'c' in link.parts:   # so it's from a private chat
-#         chat_entity = int(f'-100{link.parts[2]}')
-#     else:
-#         chat_entity = f'@{link.parts[1]}'
-#     message_id = link.parts[-1]
-#     location = f'{chat_entity}:{message_id}'
-#     await event.edit(f'`{location}`')
-#     await event.reply(str(link))
